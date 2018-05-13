a77dc066d6a1ff7790a9480cbb08d98482298ea5
Florin Gheorghe <florin_gheorghe@ymail.com>
2018-05-13 08:33:04 +0100

    working on max pairwise prod cpp

--- cpp-toolbox/max_pairwise_prod_fast/max_pairwise_prod_fast/max_pairwise_prod_fast.cpp
+++ cpp-toolbox/max_pairwise_prod_fast/max_pairwise_prod_fast/max_pairwise_prod_fast.cpp
@@ -5,44 +5,48 @@ using std::vector;
 using std::cin;
 using std::cout;
 
-int MaxPairwiseProdFast(const vector<int>& numbers) {
-	int result = 0;
+int64_t MaxPairwiseProdFast(vector<int64_t>& numbers) {
+	/* initialize result*/
+	int64_t result = 0;
+	/*get length of numbers list*/
 	int n = numbers.size();
+	/*initialize index*/
 	int index = 0;
+	/*search for max number index*/
 	for (int i = 1; i < n; ++i) {
 		if (numbers[i] > numbers[index]) {
 			index = i;
 		}
 	}
+	/*swap the last number with the max number*/
+	std::swap(numbers[n-1], numbers[index]);
 
-	# swap the last number with the max number
-	a[-1], a[index] = a[index], a[-1]
-
+	/*reset index*/
 	index = 0;
+	/*search for the next max number index*/
 	for (int i = 1; i < n-1; ++i) {
 		if (numbers[i] > numbers[index]) {
 			index = i;
 		}
 	}
 
-	# swap the second last number with the second biggest
-	a[-2], a[index] = a[index], a[-2]
+	/*/swap the second last number with the second biggest*/
+	std::swap(numbers[n-2], numbers[index]);
 
-	result = a[-1] * a[-2]
-
-
+	/*get result as the product of the last two numbers in list*/
+	result = numbers[n-2] * numbers[n-1];
 	return result;
 }
 
 int main() {
 	int n;
 	cin >> n;
-	vector<int> numbers(n);
+	vector<int64_t> numbers(n);
 	for (int i = 0; i < n; ++i) {
 		cin >> numbers[i];
 	}
 
-	int result = MaxPairwiseProdFast(numbers);
+	int64_t result = MaxPairwiseProdFast(numbers);
 	cout << result << "\n";
 	system("PAUSE");
 	return 0;
